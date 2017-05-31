e11e9a24ec8c68955bc0d94ddbd5f0669db749fa
Gary Parkin <gary.parkin@conduent.com~>
2017-05-31 12:25:01 -0400

    Add project files.

---
+++ TutorialXL/TutorialXL/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace TutorialXL
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BtnSecond_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new SecondPage();
+        }
+
+        protected override void OnAppearing()
+        {
+            DisplayAlert("Hello", "This is my main page", "Ok");
+            base.OnAppearing();
+        }
+    }
+}
---
+++ TutorialXL/TutorialXL/SecondPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TutorialXL
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SecondPage : ContentPage
+	{
+		public SecondPage ()
+		{
+			InitializeComponent ();
+		}
+        protected override bool OnBackButtonPressed()
+        {
+            App.Current.MainPage = new MainPage();
+            
+            // This traps the back button so it won't leave the app
+            //  unless it's tapped on the main page.
+            return true; // base.OnBackButtonPressed();
+        }
+    }
+}
